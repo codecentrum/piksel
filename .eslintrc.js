89f56dbdb3afa62d2915fd542f4adeb5a81a12c7
Faisal Amir <urmauur@gmail.com>
2023-12-10 23:46:39 +0700

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  ignorePatterns: [
+    'build',
+    'dist',
+    'node_modules',
+    'renderer',
+    '.next',
+    '_next',
+    '*.md',
+    'out',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@next/next/no-server-import-in-page': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'variableLike',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'property',
+        format: ['camelCase', 'snake_case', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'memberLike',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'import/order': [
+      'error',
+      {
+        'alphabetize': { order: 'asc' },
+        'groups': ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'pathGroups': [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        'pathGroupsExcludedImportTypes': ['react'],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+  },
+}
