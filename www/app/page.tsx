5af1a557254093ce5702e864138c0589ec2621f4
Faisal Amir <urmauur@gmail.com>
2023-12-15 23:54:01 +0700

    chore: add a template for landing page

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,11 +1,21 @@
+// !TODO: @urmauur
+// !this is still under development
+// ** This is a landing page
+
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat aut
-        autem est quos, illum totam numquam explicabo quia at error? Vel sit
-        saepe excepturi suscipit rem. Iste debitis quam impedit.
-      </p>
-    </div>
+    <main className="flex h-full items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-8xl font-bold">Piksel</h1>
+        <p className="mt-3 leading-relaxed">
+          Beautifully designed components that you can use for any projects.
+        </p>
+        <Link href="/docs/introduction" className="mt-4 inline-block underline">
+          See documentation
+        </Link>
+      </div>
+    </main>
   )
 }
